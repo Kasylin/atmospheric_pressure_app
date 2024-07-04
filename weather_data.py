6a59d3a28b33e4c0661b1eb44291de01bf36cec1
Alina Kasymova <alina.r.kasymova@gmail.com>
2024-07-04 10:31:03 +0300

    Add request of weather forecast

--- weather_data.py
+++ weather_data.py
@@ -56,6 +56,54 @@ def get_historical_weather(start_date, end_date):
     return pd.DataFrame(data=hourly_data)
 
 
+def get_weather_forecast(forecast_days=7):
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important
+    # to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 55.7522,
+        "longitude": 37.6156,
+        "hourly": ["temperature_2m", "relative_humidity_2m", "precipitation",
+                   "weather_code", "surface_pressure"],
+        "timezone": "Europe/Moscow",
+        "forecast_days": forecast_days
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location.
+    # Add a for-loop for multiple locations or weather models
+    response = responses[0]
+
+    # Process hourly data.
+    # The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+    hourly_precipitation = hourly.Variables(2).ValuesAsNumpy()
+    hourly_weather_code = hourly.Variables(3).ValuesAsNumpy()
+    hourly_surface_pressure = hourly.Variables(4).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+        end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+        freq=pd.Timedelta(seconds=hourly.Interval()),
+        inclusive="left"
+    )}
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+    hourly_data["precipitation"] = hourly_precipitation
+    hourly_data["weather_code"] = hourly_weather_code
+    hourly_data["surface_pressure"] = hourly_surface_pressure
+
+    return pd.DataFrame(data=hourly_data)
+
+
 def max_diff_in_window(dataframe, column='surface_pressure_mmHg', window=48):
     dataframe[column+'max_diff_backward'] = None
     dataframe[column+'max_diff_forward'] = None
